aac14ec9b8be8744f8aa8535d1bce3969539b551
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2022-12-18 18:17:22 +0000

    WIP

---
+++ app/Http/Livewire/ProjectManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ProjectManager extends Component
+{
+    public function render()
+    {
+        return view('livewire.project-manager');
+    }
+}
